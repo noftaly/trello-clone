d3480cf29c903ca367719bd9916207407cb68b22
noftaly <noftaly@gmail.com>
2020-07-03 20:04:01 +0200

    ✨ Enhance backend's terminal with chalk

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,4 +1,13 @@
+import chalk from 'chalk';
 import { createLogger, format, transports } from 'winston';
+
+const backgrounds = {
+  info: 'bgCyan',
+  error: 'bgRed',
+  success: 'bgGreen',
+  warn: 'bgYellow',
+  warning: 'bgYellow',
+};
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
@@ -6,7 +15,7 @@ const logger = createLogger({
   level: 'info',
   format: format.combine(
     format.splat(),
-    format.simple(),
+    format.printf(info => `${chalk.black[backgrounds[info.level]](` ${info.level.toUpperCase()} `)} ${info.message}`),
   ),
   transports: [
     new transports.Console(),
