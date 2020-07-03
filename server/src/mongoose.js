e462183ac8c4484772cad32e917f8811cbda2658
noftaly <noftaly@gmail.com>
2020-07-03 20:04:14 +0200

    🔥 Remove mongoose's warnings

--- server/src/mongoose.js
+++ server/src/mongoose.js
@@ -4,7 +4,7 @@ import logger from './logger';
 export default function (app) {
   mongoose.connect(
     app.get('mongodb'),
-    { useUnifiedTopology: true, useNewUrlParser: true },
+    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
   ).catch((err) => {
     logger.error(err);
     process.exit(1);
