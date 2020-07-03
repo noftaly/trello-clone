f72c0ac656bf3a589895d5611a9d2bd70f65c300
noftaly <noftaly@gmail.com>
2020-07-03 19:38:00 +0200

    💄 Update layout

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -19,89 +19,87 @@
         </v-flex>
 
         <v-flex xs12 v-else>
-          <v-row>
-            <v-progress-circular
-              v-if="loadingBoard || loadingLists"
-              :size="70"
-              :width="7"
-              color="primary"
-              indeterminate
-            ></v-progress-circular>
+          <v-progress-circular
+            v-if="loadingBoard || loadingLists"
+            :size="70"
+            :width="7"
+            color="primary"
+            indeterminate
+          ></v-progress-circular>
 
-            <div v-else>
-              <v-flex
-                sm3
-                class="mx-4 my-4"
-                v-for="list in lists"
-                :key="list._id"
+          <v-row v-else>
+            <v-flex
+              sm3
+              class="mx-4 my-4"
+              v-for="list in lists"
+              :key="list._id"
+            >
+              <v-card
+                max-width="400px"
+                @dragover="setDroppingList($event, list)"
+                :class="{ 'green lighten-5': droppingList === list }"
               >
-                <v-card
-                  max-width="400px"
-                  @dragover="setDroppingList($event, list)"
-                  :class="{ 'green lighten-5': droppingList === list }"
-                >
-                  <v-card-title primary-title>
-                    <v-layout column>
-                      <v-flex xs-12>
-                        <div class="headline">{{list.name}}</div>
-                      </v-flex>
+                <v-card-title primary-title>
+                  <v-layout column>
+                    <v-flex xs-12>
+                      <div class="headline">{{list.name}}</div>
+                    </v-flex>
 
-                      <v-flex xs12 v-for="card in cardsByListId[list._id]" :key="card._id">
-                        <v-card
-                          class="my-1 px-2"
-                          draggable="true"
-                          @dragstart="startDraggingCard(card)"
-                          @dragend="dropCard()"
-                        >
-                          <v-container>
-                            <v-layout row>
-                              <v-flex xs7>
-                                <div>{{card.title}}</div>
-                              </v-flex>
-                            </v-layout>
-                          </v-container>
-                        </v-card>
-                      </v-flex>
-                    </v-layout>
-                  </v-card-title>
-
-                  <v-card-actions>
-                    <create-card :listId="list._id" :boardId="$route.params.id"></create-card>
-                  </v-card-actions>
-                </v-card>
-              </v-flex>
-
-              <v-flex sm3 class="mx-4 my-4" >
-                <v-card max-width="400px">
-                  <v-card-title primary-title style="flex-direction: column;">
-                    <div class="headline">Create List</div>
-                    <div>
-                      <v-form
-                        v-if="!creatingList"
-                        v-model="validList"
-                        @submit.prevent="createList()"
-                        @keydown.prevent.enter
+                    <v-flex xs12 v-for="card in cardsByListId[list._id]" :key="card._id">
+                      <v-card
+                        class="my-1 px-2"
+                        draggable="true"
+                        @dragstart="startDraggingCard(card)"
+                        @dragend="dropCard()"
                       >
-                        <v-text-field
-                          v-model="list.name"
-                          :rules="notEmptyRules"
-                          label="Name"
-                          required
-                        ></v-text-field>
-                        <v-btn type="submit" :disabled="!validList">Create List</v-btn>
-                      </v-form>
-                      <v-progress-circular
-                        v-else
-                        :size="70"
-                        :width="7"
-                        color="primary"
-                        indeterminate
-                      ></v-progress-circular>
-                    </div>
-                  </v-card-title>
-                </v-card>
-              </v-flex>
-            </div>
+                        <v-container>
+                          <v-layout row>
+                            <v-flex xs7>
+                              <div>{{card.title}}</div>
+                            </v-flex>
+                          </v-layout>
+                        </v-container>
+                      </v-card>
+                    </v-flex>
+                  </v-layout>
+                </v-card-title>
+
+                <v-card-actions>
+                  <create-card :listId="list._id" :boardId="$route.params.id"></create-card>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+
+            <v-flex sm3 class="mx-4 my-4" >
+              <v-card max-width="400px">
+                <v-card-title primary-title style="flex-direction: column;">
+                  <div class="headline">Create List</div>
+                  <div>
+                    <v-form
+                      v-if="!creatingList"
+                      v-model="validList"
+                      @submit.prevent="createList()"
+                      @keydown.prevent.enter
+                    >
+                      <v-text-field
+                        v-model="list.name"
+                        :rules="notEmptyRules"
+                        label="Name"
+                        required
+                      ></v-text-field>
+                      <v-btn type="submit" :disabled="!validList">Create List</v-btn>
+                    </v-form>
+                    <v-progress-circular
+                      v-else
+                      :size="70"
+                      :width="7"
+                      color="primary"
+                      indeterminate
+                    ></v-progress-circular>
+                  </div>
+                </v-card-title>
+              </v-card>
+            </v-flex>
           </v-row>
         </v-flex>
       </v-row>
