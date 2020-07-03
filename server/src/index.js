d534ac288e7258848c7195f77d1c9e462be19867
noftaly <noftaly@gmail.com>
2020-07-03 23:37:13 +0200

    🎨 Improve logger Well i had some time to spent and didn't want to think too much so I improved the logger and made the start message look like the one of the frontend :)

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+
 import app from './app';
 import logger from './logger';
 
@@ -6,4 +8,16 @@ const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) => logger.error('Unhandled Rejection at: Promise ', p, reason));
 
-server.on('listening', () => logger.info('Feathers application started on http://%s:%d', app.get('host'), port));
+server.on('listening', () => {
+  const now = new Date();
+  const pad = x => (x.toString().length < 2 ? `0${x}` : x).toString();
+
+  logger.success(
+    `${chalk.green('Transpilled successully')}        ${chalk.gray(`${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`)}
+
+
+Server running at:
+  - Local:   ${chalk.cyan(`http://${app.get('host')}:${chalk.bold(port)}/`)}`,
+  );
+  console.log('\n'.repeat(process.stdout.rows - 7));
+});
--- server/src/logger.js
+++ server/src/logger.js
@@ -1,25 +1,37 @@
 import chalk from 'chalk';
 import { createLogger, format, transports } from 'winston';
 
-const backgrounds = {
-  info: 'bgCyan',
-  error: 'bgRed',
-  success: 'bgGreen',
-  warn: 'bgYellow',
-  warning: 'bgYellow',
+const customLevels = {
+  error: 0,
+  warning: 1,
+  success: 2,
+  info: 3,
+  debug: 4,
 };
+
+const backgrounds = {
+  error: 'bgRed',
+  warning: 'bgYellow',
+  success: 'bgGreen',
+  info: 'bgBlue',
+  debug: 'bgWhite',
+};
+
+const coloredFormat = format.printf(({ level, message }) => {
+  const bgColor = backgrounds[level];
+  return `${chalk.black[bgColor](` ${level.toUpperCase()} `)} ${message}`;
+});
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
   level: 'info',
+  levels: customLevels,
   format: format.combine(
     format.splat(),
-    format.printf(info => `${chalk.black[backgrounds[info.level]](` ${info.level.toUpperCase()} `)} ${info.message}`),
+    coloredFormat,
   ),
-  transports: [
-    new transports.Console(),
-  ],
+  transports: [new transports.Console()],
 });
 
 export default logger;
